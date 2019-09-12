fd090a39f83ca5d4e33abca6427fc47395cbe22b
liule1001 <1762219852@qq.com>
2019-09-12 16:01:45 +0800

    用户管理

---
+++ src/pages/user/user.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import './index.less'
+class User extends Component{
+  render(){
+    return(
+      <div className='user'>
+        这里是用户管理页面
+      </div>
+   )
+  }
+}
+export default User
--- src/router.js
+++ src/router.js
@@ -1,12 +1,15 @@
 import {HashRouter,Route} from 'react-router-dom'
 import App from './App';
 import React,{Component} from 'react'
+import User from './pages/user/user'
 class RootRouter extends Component{
   render(){
     return(
       <App>
         <HashRouter>
-          
+        <Switch>
+            <Route path='/user' component={User}></Route>
+          </Switch>
         </HashRouter>
       </App>
     )
